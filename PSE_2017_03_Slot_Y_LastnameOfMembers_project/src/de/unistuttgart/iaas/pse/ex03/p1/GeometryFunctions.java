9d3dc65e384b9b89162db7d0c15bcab2e46025ca
Florian <33514818+Acaledus@users.noreply.github.com>
2017-11-18 23:59:33 +0100

    Add files via upload

---
+++ PSE_2017_03_Slot_Y_LastnameOfMembers_project/src/de/unistuttgart/iaas/pse/ex03/p1/GeometryFunctions.java
@@ -0,0 +1,20 @@
+package de.unistuttgart.iaas.pse.ex03.p1;
+
+public class GeometryFunctions {
+
+	public static double computeCircumference(double r) {
+		return 0.0;
+	}
+
+	public static double pythagorenTheorem(int a, int b) {
+		return 0.0;
+	}
+
+	public static void main(String[] args) {
+		// To test
+		System.out.println(computeCircumference(-1));
+		System.out.println(computeCircumference(3));
+		System.out.println(pythagorenTheorem(2, 3));
+	}
+
+}
